32191423fec3dc5f1a66f9c8e92e9cd5ccc38a33
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 18:34:26 +0200

    ressurection :)

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
 from matplotlib import pyplot as plt
 from itertools import product
 
+__name__ = '__main__'
 odp = input('Get Bossa data? [y/n]:')
 if odp == 'y':
     tools.get_data()
 odp = input('Get Twitter data? [y/n]:')
 if odp == 'y':
-    tools.get_Twitter_data('11bitstudios', '2014-01-01', '2019-03-01')
-
-#d = pd.read_csv('saved_tweets.csv', delimiter='\t')
-
+    tools.get_Twitter_data('11bitstudios', '2022-05-03', '2022-05-04')
 
 def build_model(max_features=None, min_df=1, nb_alpha=1.0):
     vectorizer = TfidfVectorizer(max_features=max_features, min_df=min_df)
@@ -24,7 +22,6 @@ def build_model(max_features=None, min_df=1, nb_alpha=1.0):
     pred = model.predict_proba(vectorizer.transform(d_test.review))
     return {"max_features": max_features, "min_df": min_df, "nb_alpha": nb_alpha, "auc": roc_auc_score(d_test.sentiment,
                                                                                                        pred[:, 1])}
-
 
 def search_for_optimal_params():
     param_values = {
@@ -55,9 +52,6 @@ def performance(y_true, pred, color="g", ann=True):
 #'max_features': 10000, 'min_df': 1, 'nb_alpha': 0.01, 'auc': 0.8656838656838657}
 
 
-bossa_data = tools.prepare_data('11BIT')
-
-
 def finding_rocket():
     counter = 0  # we need this to skip first value
     for index, row in bossa_data.iterrows():
@@ -68,15 +62,16 @@ def finding_rocket():
         previous_close = row['Close']
 
 
-bossa_data = tools.prepare_data('11BIT')
+bossa_data = tools.prepare_data('TESTMD')
 rockets = pd.DataFrame(columns=bossa_data.columns)  # this is empty
 finding_rocket()
 print('rockets: ', rockets)
 
-d = pd.read_csv('saved_tweets.csv', delimiter='\t')
+d = pd.read_csv('saved_tweets.csv', parse_dates=['date'], delimiter='\t')
+d['date'] = tools.lookup(d['date'], format='%Y%m%d')
 
-#d = tools.enrichSavedTweets(d, rockets)
-d = tools.set_sentiment_for_given_date(d, '2018-10-12')
+d = tools.enrichSavedTweets(d, rockets)
+#d = tools.set_sentiment_for_given_date(d, '2018-10-12')
 split = 0.7
 d_train = d[:int(split*len(d))]
 d_test = d[int((1-split)*len(d)):]
--- tools.py
+++ tools.py
@@ -2,11 +2,12 @@ import zipfile, requests, os, csv
 from datetime import datetime
 from datetime import timedelta
 import pandas as pd
-import tweepy
+import tweepy as tw
 import secret
 import got3 as got
+#import GetOldTweets3 as got
 import random
-no_of_tweets_limit = 1000000
+no_of_tweets_limit = 10
 #20144 tweetów pobierało się 35 minut
 
 def get_data():
@@ -40,37 +41,48 @@ def prepare_data(ticker):
 #the result of this method is saved tweets structure csv with fake sentiment
 def get_Twitter_data(ticker, since, date_to):
 
-    auth = tweepy.OAuthHandler(secret.consumer_key, secret.consumer_secret)
+    auth = tw.OAuthHandler(secret.consumer_key, secret.consumer_secret)
     auth.set_access_token(secret.access_token, secret.access_token_secret)
-
-    tweetCriteria = got.manager.TweetCriteria().setQuerySearch(ticker).setSince(since). \
+    api = tw.API(auth, wait_on_rate_limit=True)
+    #print('twitter auth: ', auth)
+    tweet_criteria = got.manager.TweetCriteria().setQuerySearch(ticker).setSince(since). \
         setUntil(date_to).setMaxTweets(no_of_tweets_limit)  # to jest ilosc limitu tweetow
-    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+    print('criteria: ', tweet_criteria)
+    #public_tweets = api.home_timeline()
+    #for tweet in public_tweets:
+    #    print(tweet.text)
+    search_words = ticker
+    tweets = tw.Cursor(api.search_tweets,
+                       q=search_words,
+
+                       ).items(5000)
+    #tweets = got.manager.TweetManager.getTweets(tweet_criteria)
     tw_list = []
     tw_list_dates = []
     for tweet in tweets:
         tw_list.append(tweet.text.encode('utf-8'))
-        tw_list_dates.append(tweet.date)
+        tw_list_dates.append(tweet.created_at)
     df = pd.DataFrame({'review': tw_list})  # review is the body of the tweet (the actual text)
     df['date'] = pd.DataFrame({'date': tw_list_dates})
     df['sentiment'] = '0'
     df1 = df.reindex(['date', 'sentiment', 'review'], axis=1)
-    df1.to_csv('saved_tweets.csv', encoding='utf-8', index_label=False, sep='\t')
+    df1.to_csv('saved_tweets.csv', encoding='utf-8', index_label=False, sep='\t', date_format='%Y%m%d')
 
-
+#CHYBA to trzeba od nowa zrobić!
 #this method should open the saved tweets csv and using rockets dates fill the sentiments.
 #for example set the sentiment of the rocket date tweet and 2 days before to 2 and leave rest at default 0
 def enrichSavedTweets(saved_tweets, rockets):
     saved_tweets['sentiment'] = '0'
     for index, row in saved_tweets.iterrows():
-        date_tweet = datetime.strptime(str(row['date']), '%Y-%m-%d %H:%M:%S')
-        date_tweet = date_tweet.replace(second=0)
-
+        date_tweet = row['date']
+        date_tweet = date_tweet.replace(hour=0, minute=0, second=0)
         for index2, row2 in rockets.iterrows():
             date_rocket = datetime.strptime(str(row2['Date']),  '%Y%m%d')
             date_rocket = date_rocket.replace(hour=0, minute=0, second=0)
-            print ('Comaparing ', (date_tweet-timedelta(days=2)), ' and ', date_rocket)
-            if date_tweet + timedelta(days=2) == date_rocket:
+
+            print ('Comparing ', (date_tweet-timedelta(days=2)), ' and ', date_rocket)
+            if date_tweet + timedelta(days=1) == date_rocket:
                 print("date of tweet: ", date_tweet, "matches date of rocket: ", date_rocket,
                       "so this is a tweet that indicates rocket's date")
                 saved_tweets.at[index, 'sentiment'] = '1'
@@ -83,10 +95,9 @@ def enrichSavedTweets(saved_tweets, rockets):
 def set_sentiment_for_given_date(saved_tweets, given_date):
     saved_tweets['sentiment'] = '0'
     for index, row in saved_tweets.iterrows():
-        date_tweet = datetime.strptime(str(row['date']), '%Y-%m-%d %H:%M:%S')
-        date_tweet = date_tweet.replace(second=0)
-        if  given_date == date_tweet:
-            print("Given date quals tweet date")
+        date_tweet = row['date']
+        if given_date == date_tweet:
+            print("Given date equals tweet date")
             saved_tweets.at[index, 'sentiment'] = '1'
     saved_tweets.to_csv('saved_tweets.csv', encoding='utf-8', index_label=False, sep='\t')
     print('setting complete')
@@ -104,3 +115,13 @@ def normalize_feature(data, f_min=-1.0, f_max=1.0):
     factor = (f_max - f_min) / (d_max - d_min)
     normalized = f_min + (data - d_min)*factor
     return normalized, factor
+
+def lookup(date_pd_series, format=None):
+    """
+    This is an extremely fast approach to datetime parsing.
+    For large data, the same dates are often repeated. Rather than
+    re-parse these, we store all unique dates, parse them, and
+    use a lookup to convert all dates.
+    """
+    dates = {date:pd.to_datetime(date, format=format) for date in date_pd_series.unique()}
+    return date_pd_series.map(dates)
